66a11f4f2719c2181fe980c0f855dc0278abee03
SamAbdallah <saa081@student.bau.edu.lb>
2022-06-06 16:48:02 +0300

    finished api that returns random name

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class TestController extends Controller{
+    
+    public function sayHi(){
+        $message = "From DB";
+
+        return response()->json([
+            "status" => "Success",
+            "message" => $message
+        ], 200);
+    }
+
+
+
+    public function random(){
+        $arr=["Ali","Ahmad","Joseph","Mikel"];
+        echo $arr[array_rand($arr)];
+
+    }
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [TestController::class, 'sayHi'])->name("say-hi");
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/random', [TestController::class, 'random'])->name("random-");
